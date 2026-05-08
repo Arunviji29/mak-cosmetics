ea8b516e6246ef16b8417c7750e7d301bec7f27b
Arunviji29 <arunviji149@gmail.com>
2026-05-08 14:54:17 +0530

    updated service worker

--- server.js
+++ server.js
@@ -58,9 +58,7 @@ app.post("/verify-payment", (req, res) => {
     res.json({ status: "failed" });
   }
 });
-self.addEventListener('install', () => {
-  console.log("Service Worker Installed");
-});
+
 app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
